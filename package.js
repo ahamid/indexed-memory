dd4836b36cfb3983ed4623544e2b50cee1d2c813
Aaron Hamid <aaron.hamid@gmail.com>
2016-01-02 17:43:40 -0500

    initial commit

---
+++ package.js
@@ -0,0 +1,14 @@
+var profile = {
+  resourceTags: {
+    test: function (filename, mid) {
+      return /\/test\//.test(filename);
+    },
+
+    miniExclude: function (filename, mid) {
+      return /\/(?:tests|demos|docs)\//.test(filename);
+    },
+
+    amd: function (filename, mid) {
+      return /\.js$/.test(filename);
+    }
+}
